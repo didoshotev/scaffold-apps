5ac13daa08e1253f7b4f15db95e8c224b81a64a5
didoshotev <didoshotev@gmail.com>
2022-08-18 23:28:09 +0300

    feat: dice roll game

--- Game/packages/hardhat/contracts/YourContract.sol
+++ Game/packages/hardhat/contracts/YourContract.sol
@@ -2,26 +2,45 @@ pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol"; 
+
+// import "@openzeppelin/contracts/access/Ownable.sol";
 // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
 contract YourContract {
+    address public owner;
+    uint256 public price = 0.025 ether;
+    address public beneficiary = 0xb1133778bfdF62d9C042Db0cFDf61c69d642c53b;
 
-  event SetPurpose(address sender, string purpose);
+    constructor() {
+        owner = 0xb1133778bfdF62d9C042Db0cFDf61c69d642c53b;
+    }
 
-  string public purpose = "Building Unstoppable Apps!!!";
+    function diceRoll() public payable {
+        require(msg.value == price, "Incorrect amount given");
+        bytes32 predictableRandom = keccak256(
+            abi.encodePacked(block.difficulty, msg.sender, address(this))
+        );
+        uint8 roll = uint8(predictableRandom[0]) % 6;
+        console.log("Current roll: ", roll);
 
-  constructor() payable {
-    // what should we do on deploy?
-  }
+        if (roll == 5) {
+            // you get 90% of the pot
+            (bool sent, ) = msg.sender.call{
+                value: (address(this).balance * 90) / 100
+            }("");
+        } else if (roll >= 4) {
+            // you get x2 of the price
+            (bool sent, ) = msg.sender.call{value: price}("");
+            require(sent, "Failed to send Ether!");
+            msg.sender.call{value: price}("");
+        } else {
+            // you lose (money goes to the contract)
+            (bool send, ) = beneficiary.call{value: 0.005 ether}("");
+        }
+    }
 
-  function setPurpose(string memory newPurpose) public {
-      purpose = newPurpose;
-      console.log(msg.sender,"set purpose to",purpose);
-      emit SetPurpose(msg.sender, purpose);
-  }
+    // to support receiving ETH by default
+    receive() external payable {}
 
-  // to support receiving ETH by default
-  receive() external payable {}
-  fallback() external payable {}
+    fallback() external payable {}
 }
